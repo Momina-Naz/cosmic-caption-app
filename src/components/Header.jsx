e79568c8379f35d48750cc81c45dce1fd41f917e
Momina-Naz <mominanaz045@gmail.com>
2025-04-12 22:10:48 +0500

    update header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ export default function Header(){
     return(
        <header>
         <img src="logo.png" alt="cosmic caption logo" />
-        <h2>Cosmic Caption App hehehe</h2>
+        <h2>Cosmic Caption App</h2>
        </header>
     )
 }
